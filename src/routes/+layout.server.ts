35106a7a689df95edc4f61605f280b2d2b00e0a2
Jordan Rust <jordan@oxidalwave.com>
2023-06-14 15:28:21 -0500

    Fix fetch call

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,7 +1,7 @@
 import type { LayoutServerLoad } from './$types';
 
 export async function load(event) {
-	const channels = fetch('http://localhost:5173/api/channels').then((r) => r.json());
+	const channels = event.fetch('/api/channels').then((r) => r.json());
 
 	return {
 		channels,
