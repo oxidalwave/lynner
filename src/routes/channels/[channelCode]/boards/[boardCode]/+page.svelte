0649ddabc35f678e29be32067ec7a87b601a83c6
OxidalWave <IronOxide42@gmail.com>
2023-06-15 19:26:46 -0500

    Change stub text

--- src/routes/channels/[channelCode]/boards/[boardCode]/+page.svelte
+++ src/routes/channels/[channelCode]/boards/[boardCode]/+page.svelte
@@ -11,11 +11,8 @@
 	</div>
 	{#each posts as post}
 		<div class="card p-2">
-			<div>Title</div>
-			<div>TEXT GOES HERE</div>
-			<div>TEXT GOES HERE</div>
-			<div>TEXT GOES HERE</div>
-			<div>TEXT GOES HERE</div>
+			<div class="h2">Title</div>
+			<div>Chargers fired Anthony Lynn this morning, per source. Two seasons ago, Lynn led the Chargers to a 12-4 record, then this year they finished 7-9, winning their last four games, giving him a 33-31 record in LA. Chargers now become the sixth NFL team to have a head coach opening.</div>
 		</div>
 	{/each}
 </div>
